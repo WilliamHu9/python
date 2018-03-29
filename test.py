d7c7a8de9525ec45fe16a0c10bad333060f8bdcc
jiuhuahu <1481031592@qq.com>
2018-03-29 12:09:48 +0800

    基本语法训练；

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 ##九九乘法表
+
 '''
 for i in range(1,10):
   for j in range(1,i+1):
@@ -6,9 +7,58 @@ for i in range(1,10):
   print('\n')
 '''
 
+'''
 f0, f1 = 0, 1
 while f1 <= 100:
   print(f1, end = " ")
   t = f1
   f1 = t + f0
   f0 = t
+'''
+
+#print ('100+100=%d' %(100+400))
+
+#python if else loop
+
+'''
+a = 100
+if a >= 50:
+     print('the number is big')
+elif a < 50:
+     print('the number is small')
+else:
+     print("i don't know")
+'''
+
+# for in
+
+# sum = 0
+# for number in range(101):
+#      sum += number
+#      print(sum)
+
+
+#  while
+#
+# sum,n = 0,1
+# while n < 101:
+#      sum +=  n
+#      n += 1
+# print(sum)
+
+#
+# list
+#
+# friends = ['xiaoming','ergou','sanmao']
+# friends.append('dapeng') #尾部加上一个数据
+# friends.insert(0,'diyi') #首部加一个值
+#
+# print(friends)      #全输出
+# friends.pop()       #删除最后一个值
+# friends.pop(1)      #删除第一个值
+# friends[1] = 'wangbadan'  #替换一个值
+# print(friends[1])   #输出第二个值
+# print(friends[-1])  #输出最后一个值
+
+
+
